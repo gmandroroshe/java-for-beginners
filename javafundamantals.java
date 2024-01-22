0856030faa15eaa1e1d96c6d98718296c97a721e
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-01-22 17:31:54 +0530

    javadundamentals

---
+++ javafundamantals.java
@@ -0,0 +1,14 @@
+// It's a mad, mad program. 
+
+public class Cheakpoint_1 {
+
+    public static void main(String[] args){
+        System.out.print("Success\n");
+        System.out.print("Success");
+        System.out.print(" Success \n");
+        System.out.print("\nSuccess");
+
+    }
+}
+
+
