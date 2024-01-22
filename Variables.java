9534081c6a611abbcfdbd82cec89e6f760017d7c
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-01-22 15:07:34 +0530

    Variables Update

--- Variables.java
+++ Variables.java
@@ -3,8 +3,19 @@ public class Variables {
 
 
     public static void main(String[] args) {
-        String name = "John";
+        String name = "John"; //String - stores text, such as "Hello". String values are surrounded by double quotes
+        int age = 25 ; //int - stores integers (whole numbers), without decimals, such as 25 or -25
+        float myFloatNum = 5.99f; //float - stores floating point numbers, with decimals, such as 5.99f or -5.99f
+        boolean myBool = true; //stores values with two states: true or false
+        char myLetter = 'D'; // stores single characters, such as 'a' or 'D'. Char values are surrounded by single quotes
+
+
+        //outputs
         System.out.println(name);
+        System.out.println(age);
+        System.out.println(myFloatNum);
+        System.out.println(myBool);
+        System.out.println(myLetter);
     }
 
 }
