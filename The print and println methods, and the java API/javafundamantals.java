04dc50b5273c48b3b4c5fab537019fdded1399fc
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-01-22 17:38:52 +0530

    comments update

--- The print and println methods, and the java API/javafundamantals.java
+++ The print and println methods, and the java API/javafundamantals.java
@@ -1,6 +1,11 @@
 // It's a mad, mad program. 
 
-public class Cheakpoint_1 {
+
+/*
+ * Java fundamentals refer to the basic principles and concepts that form the foundation of the Java programming language. 
+ * These fundamentals are essential for understanding and writing Java programs
+ */
+public class javafundamantals {
 
     public static void main(String[] args){
         System.out.print("Success\n");
