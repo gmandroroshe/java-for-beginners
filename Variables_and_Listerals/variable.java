e02fd514e9e7b73cd1648e513e505c4e8ca519fb
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-01-24 23:30:41 +0530

    Variables_and_Listerals fill add

---
+++ Variables_and_Listerals/variable.java
@@ -0,0 +1,17 @@
+//The program has a variable
+
+package Variables_and_Listerals;
+
+public class variable {
+
+    public static void main(String[] args){
+        int value ;
+
+        value = 5;
+
+        System.out.print("The value is ");
+        System.out.print(value);
+
+    }
+    
+}
---
+++ Variables_and_Listerals/variable2.java
@@ -0,0 +1,27 @@
+//prorgram date 2024.01.23
+
+/*
+ * When double quottion marks are placed arounf the world value it bacomes a string kiteral,
+ * not a variable name 
+ */
+
+
+
+package Variables_and_Listerals;
+
+public class variable2 {
+    public static void main(String[] args){
+        int value ;
+
+        value = 5 ;
+
+        System.out.print("The value is ");
+        System.out.println("value");  //not a variable name 
+
+        //program output is (The value is value)
+        
+
+    }
+}
+
+
