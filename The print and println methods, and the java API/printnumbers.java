8202133165c02d255f6ac2601e74841691ff98df
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-01-28 09:27:10 +0530

    print numbers

---
+++ The print and println methods, and the java API/printnumbers.java
@@ -0,0 +1,10 @@
+public class printnumbers {
+
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
+
+    
+}
