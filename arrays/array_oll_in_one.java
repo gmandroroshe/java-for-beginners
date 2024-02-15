cd118e7611321db67d9e9b977cedaafedc125a22
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-02-15 19:49:21 +0530

    Create 2 arrays string and int.

---
+++ arrays/array_oll_in_one.java
@@ -0,0 +1,19 @@
+//programe date 2024.02.15
+
+package arrays;
+
+public class array_oll_in_one {
+
+    public static void main(String[] args) {
+        
+
+        String [] Names = {"Kavindu", "Maduranga", "Aponsu"};
+        int [] Numbers = {10,20,30};
+
+        //Now let's get the output of the two Arrays created
+
+        System.out.println(Names);
+        System.out.println(Numbers); 
+    }
+    
+}
