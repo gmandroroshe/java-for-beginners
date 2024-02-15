04fc25c5a98691810186f93aed93cd84a874c3af
Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-02-15 05:58:37 +0530

    Add files via upload

---
+++ OOP in java/textboxs/Main.java
@@ -0,0 +1,12 @@
+package textboxs;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        var textBox1 = new textBox();
+        textBox1.setText("Hello"); 
+        System.out.println(textBox1.text);  
+    }
+    
+}
---
+++ OOP in java/textboxs/textBox.java
@@ -0,0 +1,14 @@
+package textboxs;
+
+public class textBox {
+
+    public String text = "";
+
+    public void setText(String text){ 
+        this.text = text ; 
+    }
+    
+    public void clear (){
+        text = ""; 
+    }
+}
