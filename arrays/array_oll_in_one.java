5e25eb4207c3f8a8a96f03f24cefa5ce95e19a93
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-02-15 20:04:00 +0530

    Change an Array Element

--- arrays/array_oll_in_one.java
+++ arrays/array_oll_in_one.java
@@ -5,22 +5,18 @@ package arrays;
 public class array_oll_in_one {
 
     public static void main(String[] args) {
-        
-
         String [] Names = {"Kavindu", "Maduranga", "Aponsu"};
         int [] Numbers = {10,20,30};
-
         //Now let's get the output of the two Arrays created
-
-        System.out.println(Names);
-        System.out.println(Numbers); 
-
+        System.out.println(Names[0]);
+        System.out.println(Numbers[0]); 
         // Access the Elements of an Array 
-
         System.out.println(Names[0]);
         System.out.println(Numbers[0]);
-
-        
+        //Change an Array Element
+        Numbers [0] = 50;
+        //now 0 index is changed 
+        System.out.println(Numbers[0]);
     }
     
 }
