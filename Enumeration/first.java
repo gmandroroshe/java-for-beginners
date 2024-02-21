234ed7bffb98846b655ace4f20dbfd374247b148
kavindu Maduranga <kavindumadurangaaponsu@gmail.com>
2024-02-21 10:52:25 +0530

    enum

--- Enumeration/first.java
+++ Enumeration/first.java
@@ -1,10 +1,20 @@
 package Enumeration;
 
+enum pepole {
+    KAVINDU 
+}
+
 public class first {
+    enum world {
+        HELLO_WORLD
+    }
+    
 
     public static final String NAME = "kavindu";
     public static void main(String[] args) {
         System.out.println(NAME);
+        System.out.println(pepole.KAVINDU);
+        System.out.println(world.HELLO_WORLD); 
     }
     
 }
